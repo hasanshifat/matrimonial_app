05facd850db4f3643dffc0f56051643621dc0695
Fusion Apps Developer <fusionappsdeveloper@Fusions-MacBook-Air.local>
2023-02-07 22:20:27 +0600

    shifat: first direct commit to dev

--- lib/Utils/snackbars.dart
+++ lib/Utils/snackbars.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class CustomSnackBars {
-  void showSnackBar(BuildContext context, String text,Color backgroundColor) {
+  void showSnackBar(BuildContext context, String text, Color backgroundColor) {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(
         behavior: SnackBarBehavior.floating,
         elevation: 0,
         duration: const Duration(seconds: 3),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
-        backgroundColor:backgroundColor ,
+        backgroundColor: backgroundColor,
         content: Text(
           text,
           style: GoogleFonts.anekBangla(
@@ -18,3 +18,4 @@ class CustomSnackBars {
       ),
     );
   }
+}
